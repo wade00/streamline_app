55272b4e3646276dc4c3116b3bbc88a2dc425874
Wade Novak <wade.novak@gmail.com>
2015-04-22 11:22:02 -0500

    Adds machine model validations and CSS tweaks

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,3 +1,15 @@
 class Machine < ActiveRecord::Base
   belongs_to :dealership
+  validates :stock_number, :make, :model, :serial_number, presence: true
+  validates :stock_number, :serial_number, uniqueness: true
+
+  before_save :format_make_model_and_type
+
+  private
+
+  def format_make_model_and_type
+    self.make.titleize
+    self.machine_model.uppercase
+    self.machine_type.titleize
+  end
 end
