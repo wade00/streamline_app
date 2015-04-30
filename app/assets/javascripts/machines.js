5be5e032b8d74da1fd9f156303dc5ae83f38c641
Wade Novak <wade.novak@gmail.com>
2015-04-30 11:01:43 -0500

    Fixes edit form JS animation on machine index page

--- app/assets/javascripts/machines.js
+++ app/assets/javascripts/machines.js
@@ -1,31 +1,33 @@
 $(document).ready(function(){
   $('.machine-preview-cell').click( function() {
-    $('.table-container-wide').toggleClass('table-container-narrow', 1000, "easeOutSine");
+    $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
 
     $('#edit-form-container').css({ margin: '0px',
+                                    height: '100vh',
                                     padding: '2em',
-                                    position: 'fixed',
+                                    position: 'absolute',
                                     right: '0',
+                                    top: '0',
                                     width: '64%' });
 
-    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 1000, "easeOutSine");
+    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
 
-    $('.description-cell').toggleClass('description-cell-hide', 1000, "easeOutSine");
+    $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
   });
 });
 
 $(document).ready(function(){
   $('.description-cell').click( function() {
-    $('.table-container-wide').toggleClass('table-container-narrow', 1000, "easeOutSine");
+    $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
 
     $('#edit-form-container').css({ margin: '0px',
                                     padding: '2em',
-                                    position: 'fixed',
+                                    position: 'absolute',
                                     right: '0',
                                     width: '64%' });
 
-    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 1000, "easeOutSine");
+    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
 
-    $('.description-cell').toggleClass('description-cell-hide', 1000, "easeOutSine");
+    $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
   });
 });
