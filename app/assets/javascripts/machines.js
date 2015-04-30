c6fa73b43376ebeaa54341b7df4486ef96662d61
Wade Novak <wade.novak@gmail.com>
2015-04-30 14:34:44 -0500

    Adds blur to unselected machine on index page

--- app/assets/javascripts/machines.js
+++ app/assets/javascripts/machines.js
@@ -1,47 +1,26 @@
 $(document).ready(function(){
-  $('.machine-preview-cell').click( function() {
+
+  $('.sidebar-preview').click( function() {
     $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
-
-    $('#edit-form-container').css({ margin: '0px',
-                                    height: '100vh',
-                                    padding: '2em',
-                                    position: 'absolute',
-                                    right: '0',
-                                    top: '0',
-                                    width: '64%' });
-
+    $('#edit-form-container').html('');
+    $('#edit-form-container').toggleClass('edit-form-container-show');
     $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
-
     $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
+    $('.sidebar-preview').toggleClass('sidebar-preview-blur', 600, "easeOutSine");
+    $('.edit-machine-link').toggleClass('unactive-link');
+    $(this).toggleClass('sidebar-preview-override');
   });
+
 });
 
-$(document).ready(function(){
-  $('.description-cell').click( function() {
-    $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
+// $(document).ready(function(){
+//   $('.cancel-machine-form').click( function() {
+//     $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
 
-    $('#edit-form-container').css({ margin: '0px',
-                                    height: '100vh',
-                                    padding: '2em',
-                                    position: 'absolute',
-                                    right: '0',
-                                    top: '0',
-                                    width: '64%' });
+//     $('#edit-form-container').hide();
 
-    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
+//     $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
 
-    $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
-  });
-});
-
-$(document).ready(function(){
-  $('.cancel-machine-form').click( function() {
-    $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
-
-    $('#edit-form-container').hide();
-
-    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
-
-    $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
-  });
-});
+//     $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
+//   });
+// });
