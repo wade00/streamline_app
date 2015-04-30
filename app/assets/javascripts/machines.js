0cffcdd2c1504f38e4c265e2f8fa610b98d64822
Wade Novak <wade.novak@gmail.com>
2015-04-30 11:38:15 -0500

    Adds responders gem and configures flash messages responder

--- app/assets/javascripts/machines.js
+++ app/assets/javascripts/machines.js
@@ -21,10 +21,24 @@ $(document).ready(function(){
     $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
 
     $('#edit-form-container').css({ margin: '0px',
+                                    height: '100vh',
                                     padding: '2em',
                                     position: 'absolute',
                                     right: '0',
+                                    top: '0',
                                     width: '64%' });
+
+    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
+
+    $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
+  });
+});
+
+$(document).ready(function(){
+  $('.cancel-machine-form').click( function() {
+    $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
+
+    $('#edit-form-container').hide();
 
     $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
 
