add030f53bfdd5aebafc5ff940220dba62a1ff04
Wade Novak <wade.novak@gmail.com>
2015-05-01 13:45:08 -0500

    Adds javascript actions for cancel and save edit actions

--- app/assets/javascripts/machines.js
+++ app/assets/javascripts/machines.js
@@ -1,26 +1,16 @@
 $(document).ready(function(){
+  $('.machine-preview').click( function() {
+    $('.machine-stats').toggleClass('hide-element');
+    $('.table-container-wide').toggleClass('table-container-narrow', 600, 'easeOutSine');
+    $('.sort-header').toggleClass('sort-header-narrow', 600, 'easeOutSine');
 
-  $('.sidebar-preview').click( function() {
-    $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
     $('#edit-form-container').html('');
     $('#edit-form-container').toggleClass('edit-form-container-show');
-    $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
-    $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
-    $('.sidebar-preview').toggleClass('sidebar-preview-blur', 600, "easeOutSine");
     $('.edit-machine-link').toggleClass('unactive-link');
-    $(this).toggleClass('sidebar-preview-override');
+
+    $('.machine-preview').toggleClass('machine-preview-blur', 600, 'easeOutSine');
+    $('.machine-preview').toggleClass('unactive-link');
+    $(this).toggleClass('machine-preview-override');
+    $(this).toggleClass('unactive-link');
   });
-
 });
-
-// $(document).ready(function(){
-//   $('.cancel-machine-form').click( function() {
-//     $('.table-container-wide').toggleClass('table-container-narrow', 600, "easeOutSine");
-
-//     $('#edit-form-container').hide();
-
-//     $('.machine-preview-cell').toggleClass('machine-preview-cell-narrow', 600, "easeOutSine");
-
-//     $('.description-cell').toggleClass('description-cell-hide', 100, "easeOutSine");
-//   });
-// });
