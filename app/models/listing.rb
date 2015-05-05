85c1694ed3615c303d47b6886f7a1eae2213e007
Wade Novak <wade.novak@gmail.com>
2015-05-05 15:33:26 -0500

    Adds search to machine index page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ActiveRecord::Base
+  # enum websites: [ :machine_trader, :equip_alley, ... ]
+
   belongs_to :owner, foreign_key: :user_id, class_name: "User"
   belongs_to :machine
 end
