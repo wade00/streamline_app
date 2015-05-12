e5ff225e3f9f81a46d3df06f31ab8c22f04ff63e
Wade Novak <wade.novak@gmail.com>
2015-05-12 16:27:37 -0500

    Improves listing update flow

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
     $(this).val('√');
     $(this).removeClass('btn-primary');
   });
+
+  $('body').on('click', '.listing-show-more', function() {
+    var listingId = $(this).attr('id');
+    $('#' + listingId + '_stats').slideToggle();
+
+    if($(this).html() == '<i class="fa fa-minus-square"></i>') {
+      $(this).html('<i class="fa fa-plus-square"></i>');
+    } else {
+      $(this).html('<i class="fa fa-minus-square"></i>');
+    }
+  });
 });
