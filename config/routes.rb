17fbb9eaee95b33f640ab0093fa94eec8f5f993f
Wade Novak <wade.novak@gmail.com>
2015-05-19 11:12:36 -0500

    Removes porthole test

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'listings/equip_alley',   { controller: :listings, action: :equip_alley }
   get 'listings/equip_locator', { controller: :listings, action: :equip_locator }
   get 'listings/mach_trader',   { controller: :listings, action: :mach_trader }
+
 end
